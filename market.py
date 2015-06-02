b3b7bdc8acd5bd5b7ef9acb898a4bbc232e92224
nodice73 <adam.james.waite@gmail.com>
2015-06-01 23:31:35 -0400

    fixed thread count and added keywords.

--- market.py
+++ market.py
@@ -8,6 +8,22 @@ total_replies = 0
 three_plus = 0
 files = ['/home/nodice/Desktop/revised questions and answers for article.csv',
          '/home/nodice/Desktop/new messages.csv']
+keywords = ['regulations',
+            'expert',
+            'health',
+            'poverty',
+            'class',
+            'privilege',
+            'alternative food',
+            'legitimate',
+            'leadership',
+            'leader',
+            'learning',
+            'outreach',
+            'equality',
+            'unity',
+            'progress',
+            'growth']
 
 for file in files:
     with open(file) as csvfile:
@@ -24,13 +40,13 @@ for file in files:
                     word_count = len(re.findall('\w+', m.group(1)))
                     if word_count < 4:
                         thread_replies += 1
-                        total_replies += 1
                         if m.group(1) in responders:
                             responders[m.group(1)] += 1
                         else:
                             responders[m.group(1)] = 1
-                    if thread_replies >= 3:
-                        three_plus += 1
+            total_replies += thread_replies
+            if thread_replies >= 3:
+                three_plus += 1
 
 pp = pprint.PrettyPrinter(indent=4)
 responders = sorted(responders.items(), key=lambda x:x[1], reverse=True)
