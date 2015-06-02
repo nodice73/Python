e79054ae8b3f27adb3e938faf4700714397bf44e
nodice73 <adam.james.waite@gmail.com>
2015-06-01 23:22:00 -0400

    added count of threads with 3+ responses.

--- market.py
+++ market.py
@@ -4,7 +4,8 @@ import csv, re, pprint
 
 responders = dict()
 n_threads = 0 
-n_replies = 0
+total_replies = 0
+three_plus = 0
 files = ['/home/nodice/Desktop/revised questions and answers for article.csv',
          '/home/nodice/Desktop/new messages.csv']
 
@@ -13,22 +14,27 @@ for file in files:
         r = csv.reader(csvfile)
         for row in r:
             n_threads += 1
-            responses = row[3].split('\n')
-            for response in responses:
-                m = re.match('^([^\s|\d]+(\w|\s)+?):', response)
+            thread_replies = 0
+            response_lines = row[3].split('\n')
+            for line in response_lines:
+                m = re.match('^([^\s|\d]+(\w|\s)+?):', line)
                 if m:
                     if re.search('http', m.group(1)):
                         continue
                     word_count = len(re.findall('\w+', m.group(1)))
                     if word_count < 4:
-                        n_replies += 1
+                        thread_replies += 1
+                        total_replies += 1
                         if m.group(1) in responders:
                             responders[m.group(1)] += 1
                         else:
                             responders[m.group(1)] = 1
+                    if thread_replies >= 3:
+                        three_plus += 1
 
 pp = pprint.PrettyPrinter(indent=4)
 responders = sorted(responders.items(), key=lambda x:x[1], reverse=True)
 print 'threads: {}, replies: {}. {:.2f} replies per thread.'.format(n_threads,
-        n_replies, float(n_replies)/n_threads)
+      total_replies, float(total_replies)/n_threads)
+print 'threads with 3+ responses: {}'.format(three_plus)
 pp.pprint(responders)
